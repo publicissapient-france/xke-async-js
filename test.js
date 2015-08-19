9284d6ed2f699e8e7cf00b94e370354ee64c0045
Florent Le Gall <florent.legall@gmail.com>
2015-08-19 18:44:05 +0200

    Add simple text query

--- test.js
+++ test.js
@@ -1,2 +1,17 @@
-const test = () => console.log('test');
-test();
+const rp = require('request-promise');
+const _ = require('lodash');
+const BlueBird = require('bluebird');
+
+function getList() {
+  return rp('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
+}
+
+function getNews(newsId) {
+  console.log(newsId);
+  return rp(`https://hacker-news.firebaseio.com/v0/item/${newsId}.json?print=pretty`);
+}
+
+getList()
+  //.then((topNews) => getNews(_.first(topNews)))
+  .then(console.log)
+  .done();
